96b0fd801aa9ea9e8742cf0d781889828df6a92d
Supanut Laddayam <ynotmela@gmail.com>
2020-11-02 13:46:13 +0700

    use mongoAtlas

--- src/db.js
+++ src/db.js
@@ -5,4 +5,4 @@ const option = {
     useUnifiedTopology: true
 }
 
-mongoose.connect('mongodb://localhost:27017/spockCafe', option)
+mongoose.connect( "mongodb+srv://root:root@cluster0.l6dmp.mongodb.net/cafe?retryWrites=true&w=majority", option)
